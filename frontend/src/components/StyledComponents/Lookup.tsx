762d90a1b18a7a4a91368d7cb4a0b67d8d0ce9ae
Afraz Aslam <m.afrazaslam@gmail.com>
2024-08-04 20:38:49 -0700

    worked on some code refactoring

--- frontend/src/components/StyledComponents/Lookup.tsx
+++ frontend/src/components/StyledComponents/Lookup.tsx
@@ -1,22 +1,18 @@
 import styled from "styled-components";
-import { colors, commonInputStyles } from "./Common";
+import { colors } from "./Common";
 import { Button } from "./Button";
 
-export const LookupInput = styled.input`
-  ${commonInputStyles};
-`;
-
 export const LookupButton = styled(Button)`
-  background-color: ${colors.green};
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 8px 16px;
-  font-size: 14px;
-  cursor: pointer;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+	background-color: ${colors.green};
+	color: white;
+	border: none;
+	border-radius: 4px;
+	padding: 8px 16px;
+	font-size: 14px;
+	cursor: pointer;
+	box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
 
-  &:hover {
-    background-color: ${colors.lightBlue};
-  }
+	&:hover {
+		background-color: ${colors.lightBlue};
+	}
 `;
--- frontend/src/pages/LookupPage.tsx
+++ frontend/src/pages/LookupPage.tsx
@@ -1,74 +1,79 @@
-import { useLocation } from 'react-router-dom';
-import StudentCard from '../components/Student/StudentDescriptionCard';
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useState, useEffect, useCallback } from 'react';
-import { RootState, AppDispatch } from '../redux/store';
-import { Container } from '../components/StyledComponents/Common';
-import { fetchStudentById } from '../redux/student/studentService';
-import { LookupButton, LookupInput } from '../components/StyledComponents/Lookup';
-import { Student } from '../types';
-import { setSelectedStudent } from '../redux/student/reducer';
-
+import { useLocation } from "react-router-dom";
+import StudentCard from "../components/Student/StudentDescriptionCard";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect, useCallback } from "react";
+import { RootState, AppDispatch } from "../redux/store";
+import { H2 } from "../components/StyledComponents/Common";
+import { fetchStudentById } from "../redux/student/studentService";
+import { Student } from "../types";
+import { setSelectedStudent } from "../redux/student/reducer";
+import { FlexedCenterContainer } from "../components/StyledComponents/FixedCenterContainer";
+import SearchStudents from "../components/Student/SearchStudents";
 
 const LookupPage: React.FC = () => {
-  const location = useLocation();
-  const [uuid, setUuid] = useState<number | undefined>(undefined);
-  const dispatch = useDispatch<AppDispatch>();
-  const students = useSelector((state: RootState) => state.student.students);
-  const selectedStudent = useSelector((state: RootState) => state.student.selectedStudent);
+	const location = useLocation();
+	const [uuid, setUuid] = useState<number | undefined>(undefined);
+	const dispatch = useDispatch<AppDispatch>();
+	const students = useSelector((state: RootState) => state.student.students);
+	const selectedStudent = useSelector(
+		(state: RootState) => state.student.selectedStudent
+	);
 
-  useEffect(() => {
-    const query = new URLSearchParams(location.search);
-    const uuidParam = query.get('uuid');
-    if (uuidParam) {
-      setUuid(parseInt(uuidParam));
-    }
-  }, [location.search]);
+	useEffect(() => {
+		const query = new URLSearchParams(location.search);
+		const uuidParam = query.get("uuid");
+		if (uuidParam) {
+			setUuid(parseInt(uuidParam));
+		}
+	}, [location.search]);
 
-  const handleLookup = useCallback(() => {
-    if (uuid === undefined) return;
+	const handleLookup = useCallback(() => {
+		if (uuid === undefined) return;
 
-    const studentInState = students.find((student: Student) => student.uuid === uuid);
+		const studentInState = students.find(
+			(student: Student) => student.uuid === uuid
+		);
 
-    if (studentInState) {
-      dispatch(setSelectedStudent(studentInState));
-    } else {
-      dispatch(fetchStudentById(uuid))
-        .then((action) => {
-          if (fetchStudentById.fulfilled.match(action)) {
-            dispatch(setSelectedStudent(action.payload));
-          } else {
-            dispatch(setSelectedStudent(null));
-          }
-        });
-    }
-  }, [uuid, students, dispatch]);
+		if (studentInState) {
+			dispatch(setSelectedStudent(studentInState));
+		} else {
+			dispatch(fetchStudentById(uuid)).then((action) => {
+				if (fetchStudentById.fulfilled.match(action)) {
+					dispatch(setSelectedStudent(action.payload));
+				} else {
+					dispatch(setSelectedStudent(null));
+				}
+			});
+		}
+	}, [uuid, students, dispatch]);
 
-  useEffect(() => {
-    if (uuid !== undefined) {
-      handleLookup();
-    }
-  }, [uuid, handleLookup]);
+	useEffect(() => {
+		if (uuid !== undefined) {
+			handleLookup();
+		}
+	}, [uuid, handleLookup]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUuid(e.target.value ? parseInt(e.target.value) : undefined);
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUuid(e.target.value ? parseInt(e.target.value) : undefined);
+	};
 
-  return (
-    <Container>
-      <h1>Search Student</h1>
-      <LookupInput
-        type="number"
-        value={uuid !== undefined ? uuid : ''}
-        onChange={handleChange}
-        placeholder="Enter Student UUID"
-      />
-      <LookupButton onClick={handleLookup}>Lookup</LookupButton>
-      {selectedStudent && (
-        <StudentCard student={selectedStudent} />
-      )}
-    </Container>
-  );
+	return (
+		<FlexedCenterContainer height="90vh" direction="column">
+			<h1>Search Student</h1>
+			<FlexedCenterContainer height="3rem">
+				<SearchStudents
+					searchTerm={uuid !== undefined ? uuid.toString() : ""}
+					onSearchSubmit={handleChange}
+					onSearchChange={handleChange}
+				></SearchStudents>
+			</FlexedCenterContainer>
+			{selectedStudent ? (
+				<StudentCard student={selectedStudent} />
+			) : (
+				<H2>"No Record Found"</H2>
+			)}
+		</FlexedCenterContainer>
+	);
 };
 
 export default LookupPage;
